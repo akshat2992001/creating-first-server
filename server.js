8b9ac3584ffdd3990740512a767862aff7bd9984
akshat2992001 <akshat.inn@gmail.com>
2022-08-29 05:05:27 +0530

    Created Routes in Servers and Methods

--- server.js
+++ server.js
@@ -2,11 +2,47 @@ const http = require("http");
 
 const port = 8081;
 
+const toDolist = ["Complete Node Byte", "Play Cricket"];
+
 http
 .createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write("<h1>Hello , this is my server</h1>")
+    const { method, url } = request;
+
+    if (url === "/todos") {
+        if (method === "GET") {
+            response.writeHead(200, { "Content-type": "text/html" });
+            response.write(toDolist.toString());
+        }
+        else if (method === "POST") {
+            let body = "";
+            request.on('error',(err) => {
+                console.error(err)
+
+
+            }).on('data', (chunk) => {
+                body += chunk;
+                
+
+            })
+            .on('end', () => {
+                body = JSON.parse(body);
+                console.log("data: ",body);
+            });
+
+        }
+
+        else {
+            response.writeHead(501);
+        }
+        
+    }
+    else 
+    {
+        response.writeHead(404);
+    }
+
     response.end();
+   
 })
 .listen(port, () => {
     console.log(`Nodejs server started successfully on port ${port}`);
